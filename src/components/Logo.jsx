04376994e17890e88e0527e111f02a4c6624e75a
sachinn-nayak <sachinnayak9108@gmail.com>
2024-08-20 21:05:23 +0530

    all  commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '70px'}) {
+  return (
+    <div>
+      <img src="https://tse2.mm.bing.net/th?id=OIP.WFSVR9iaq6ADFxn8NFEYhgHaKQ&pid=Api&P=0&h=220" alt="" srcset="" width = '70px' />
+    </div>
+  )
+}
+
+export default Logo
